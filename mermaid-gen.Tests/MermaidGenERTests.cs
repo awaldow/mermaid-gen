2b716a408a2dd671c7b8056ef2ca469260ad416f
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-22 08:23:17 -0700

    add test project

---
+++ mermaid-gen.Tests/MermaidGenERTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace mermaid_gen.Tests
+{
+    public class MermaidGenERTests
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
