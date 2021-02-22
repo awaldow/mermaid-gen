e23173906a4d6da8619c850309f73b0c5e49e67d
Addison Waldow <addison.waldow@phoenix.gov>
2021-02-22 08:13:46 -0700

    init

---
+++ mermaid-gen/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace mermaid_gen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
